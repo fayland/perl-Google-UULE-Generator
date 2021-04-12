890a3e8783c701f01f61855624ee0bbf31f9fd42
Fayland Lam <fayland@gmail.com>
2021-04-12 15:48:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Google::UULE::Generator - Blah blah blah
+
+# SYNOPSIS
+
+    use Google::UULE::Generator;
+
+# DESCRIPTION
+
+Google::UULE::Generator is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2021- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Google/UULE/Generator.pm
@@ -0,0 +1,39 @@
+package Google::UULE::Generator;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::UULE::Generator - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Google::UULE::Generator;
+
+=head1 DESCRIPTION
+
+Google::UULE::Generator is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2021- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
